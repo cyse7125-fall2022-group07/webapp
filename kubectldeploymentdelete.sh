32662c1e42079bbdcf482d9f1f76142895825384
Vinit Harsora <harsora.v@northeastern.edu>
2022-11-05 00:40:24 -0400

    Modifying to new changes

---
+++ kubectldeploymentdelete.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kubectl delete -n kubernetes-dashboard deployment todo-app
+kubectl delete -n kubernetes-dashboard configmap todo-app-config
+kubectl delete -n kubernetes-dashboard secret todo-app-secret
+kubectl delete -n kubernetes-dashboard service todo-app
